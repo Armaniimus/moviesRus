adb310d51acbfe9e39a8d8bb67cb315ade4bf18f
Armand_laptop <517226@edu.rocmn.nl>
2018-12-15 02:14:32 +0100

    added acteurnamen en rating aan de clear functie

--- view/javascript/javaScript.js
+++ view/javascript/javaScript.js
@@ -5,5 +5,8 @@ function clearform() {
         document.getElementById('day').value = "";
         document.getElementById('month').value = "";
         document.getElementById('year').value = "";
+        document.getElementById('act_fname').value = "";
+        document.getElementById('act_lname').value = "";
+        document.getElementById('rating').value = "";
     }, 1);
 }
