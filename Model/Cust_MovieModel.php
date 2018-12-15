f6056ac2b9f255d74ea52d6a1401f788b9a0ae4e
Armand_laptop <517226@edu.rocmn.nl>
2018-12-15 01:55:01 +0100

    Cust_MovieModel beoordeling en actor toegevoegt

--- Model/Cust_MovieModel.php
+++ Model/Cust_MovieModel.php
@@ -14,11 +14,10 @@
                 $andBindingsArray = [];
                 $andString = '';
 
-
-
                 if (isset($_POST['name'])) {
                     $name = trim($_POST['name'], " ");
-                    $andBindingsArray['name'] = "%$name%";
+                    $andBindingsArray['name'] = "%" . $name . "%";
+                    $_SESSION['name'] = $name;
                 } else {
                     $andBindingsArray['name'] = "%%";
                 }
@@ -27,24 +26,27 @@
                 if (isset($_POST['year'])) {
                     $year = trim($_POST['year'], " ");
                     if ($year !== "") {
-                        $andBindingsArray['year'] = $_POST['year'];
+                        $andBindingsArray['year'] = $year;
                         $andString .= " AND mov_year = :year";
+                        $_SESSION["year"] = $year;
                     }
                 }
 
                 if (isset($_POST['month'])) {
                     $month = trim($_POST['month'], " ");
                     if ($month !== "") {
-                        $andBindingsArray['month'] = $_POST['month'];
+                        $andBindingsArray['month'] = $month;
                         $andString .= " AND MONTH(mov_dt_rel) = :month";
+                        $_SESSION["month"] = $month;
                     }
                 }
 
                 if (isset($_POST['day'])) {
                     $day = trim($_POST['day'], " ");
                     if ($day !== "") {
-                        $andBindingsArray['day'] = $_POST['day'];
+                        $andBindingsArray['day'] = $day;
                         $andString .= " AND DAY(mov_dt_rel) = :day";
+                        $_SESSION["day"] = $day;
                     }
                 }
 
@@ -55,18 +57,32 @@
 
             if (isset($_POST['submit'])) {
                 $data = $this->DataHandler->readData(
-                    "SELECT mov_title AS 'Titel', mov_dt_rel AS 'Release date', mov_time AS 'Film lengte'
+                    "SELECT
+                        mov_title AS 'Titel'
+                        ,DATE_FORMAT(mov_dt_rel, '%d-%m-%Y' ) AS 'Datum van uitgave'
+                        ,mov_time AS 'Film lengte'
+                        ,rev_stars AS 'Beoordeling'
+                        ,act_fname AS 'Voornaam acteur'
+                        ,act_lname AS 'Achternaam acteur'
                     FROM movie
+                    LEFT JOIN movie_cast USING(mov_id)
+                    LEFT JOIN actor USING(act_id)
+                    LEFT JOIN rating USING(mov_id)
                     WHERE mov_title LIKE :name
                     $andString
+                    ORDER BY mov_title
                     ",
                     $andBindingsArray
                 );
 
 
                 if ($data) {
+                    $_SESSION['resCount'] = count($data);
                     for ($i=0; $i < count($data); $i++) {
                         $data[$i]["Film lengte"] = $data[$i]["Film lengte"] . " min";
+                        if ($data[$i]["Datum van uitgave"] == "00-00-0000") {
+                            $data[$i]["Datum van uitgave"] = "";
+                        }
                     }
 
                     return $this->htmlElements->advancedTable($data, 'result-table', [1,1,0]);
