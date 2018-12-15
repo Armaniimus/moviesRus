ec615685d1f3a9c667f27db78a4d5a1c7aaaa5e1
Armand_laptop <517226@edu.rocmn.nl>
2018-12-15 02:04:12 +0100

    SessionVariables aangemaakt

--- Router/config.php
+++ Router/config.php
@@ -8,4 +8,17 @@ Define("DB_USERNAME", "root");
 Define("DB_PASS", "");
 Define("DB_SERVER_ADRESS", "localhost");
 Define("DB_TYPE", "mysql");
+
+$sessionModel = new SessionModel();
+$sessionModel->sessionSupport();
+
+
+$_SESSION['resCount'] = NULL;
+$_SESSION['name'] = NULL;
+$_SESSION['day'] = NULL;
+$_SESSION['month'] = NULL;
+$_SESSION['year'] = NULL;
+$_SESSION['act_fname'] = NULL;
+$_SESSION['act_lname'] = NULL;
+$_SESSION['rating'] = NULL;
 ?>
